474c7f2764e3fa368f26b05a02a9517e5383fca5
TechDev-CEO <shoriful521399@gmail.com>
2022-01-20 12:04:54 +0600

    practice with cdn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
+
+
+const items = [
+  {
+    name: 'Abudl eto din ki korli', description: "Kita kiti bangladesh humayon aid kita kiti bangladesh", img: 'https://images.gr-assets.com/authors/1456905354p8/15050617.jpg'
+  },{
+    name: 'Abudl eto din ki korli', description: "Kita kiti bangladesh humayon aid kita kiti bangladesh", img: 'https://images.gr-assets.com/authors/1456905354p8/15050617.jpg'
+  },{
+    name: 'Abudl eto din ki korli', description: "Kita kiti bangladesh humayon aid kita kiti bangladesh", img: 'https://images.gr-assets.com/authors/1456905354p8/15050617.jpg'
+  },{
+    name: 'Abudl eto din ki korli', description: "Kita kiti bangladesh humayon aid kita kiti bangladesh", img: 'https://images.gr-assets.com/authors/1456905354p8/15050617.jpg'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="row row-cols-1 row-cols-md-2 g-4">
+       {
+         items.map(item=><Card
+         item={item}
+         >
+         </Card>)
+       }
     </div>
   );
 }
